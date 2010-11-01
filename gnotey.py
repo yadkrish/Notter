20c203e527a0fc6bb4596358fe2fbe7c7f8df09c
Yadav Krishna <yadav.krishna@gmail.com>
2010-11-01 23:47:23 +0530

    Edit of titles completed. Working on including modifcation date in display list

--- gnotey.py
+++ gnotey.py
@@ -5,6 +5,7 @@ import gtk
 import sqlite3
 import sys
 import os.path
+import datetime
 
 pygtk.require("2.0")
 
@@ -17,7 +18,7 @@ class GNotey(object):
       builder.add_from_file( os.path.join( sys.path[0], "notey.xml" ) )
       builder.connect_signals(self)
 
-      #
+      
       # GtkBuilder is not really good at treeviews
       # so we need to create our own cells and associate them
       # see http://faq.pygtk.org/index.py?req=show&file=faq13.039.htp
@@ -25,8 +26,11 @@ class GNotey(object):
       #
       cell0 = gtk.CellRendererText()
       self.col0 = gtk.TreeViewColumn("Title", cell0,text=0)
+      cell1 = gtk.CellRendererText()
+      self.col1 = gtk.TreeViewColumn("Modified Date", cell1,text=1)
       self.treeview1 = builder.get_object("treeview1")
       self.treeview1.append_column(self.col0)
+      self.treeview1.append_column(self.col1)      
       self.treeview1.get_selection().set_mode(gtk.SELECTION_MULTIPLE)
       self.liststore1 = builder.get_object("liststore1")
       
@@ -36,7 +40,7 @@ class GNotey(object):
       self.textbuffer1 = builder.get_object("textbuffer1")
       self.textview1 = builder.get_object("textview1")
 
-      self.window2 = builder.get_object("window2")
+      self.window2 = builder.get_object("window2")			
       self.entry2 = builder.get_object("entry2")
 
       self.entry1 = builder.get_object("entry1")
@@ -47,31 +51,63 @@ class GNotey(object):
       
       self.window1.show()
 
+      
   def setup_db(self):
       if ( not os.path.isfile(self.notedb) ):
         conn = sqlite3.connect(self.notedb)
         c = conn.cursor()
-        c.execute("create table notes ( title text UNIQUE, content text )")
+        c.execute("create table notes ( title text UNIQUE, mod_date text, content text )")
         conn.commit()
         c.close()
       else:
         pass
+
+  def responseToDialog(self,entry, dialog, response):
+    dialog.response(response)
+    
+  def getNewTitle(self,currentTitle=""):
+    dialog = gtk.MessageDialog(None,
+      gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+      0,
+      gtk.BUTTONS_OK,
+      None)
+
+    entry = gtk.Entry()
+    entry.set_text(currentTitle)
+    entry.connect("activate", self.responseToDialog, dialog, gtk.RESPONSE_OK)
+
+    hbox = gtk.HBox()
+    hbox.pack_start(gtk.Label("New title:"), False, 5, 5)
+    hbox.pack_end(entry)
+  
+    dialog.vbox.pack_end(hbox, True, True, 0)
+    dialog.show_all()
+
+    dialog.run()
+    text = entry.get_text()
+    self.entry1.set_text(text)
+    dialog.destroy()
+    return text
 
   def populate_liststore1(self):
       """
 populate the liststore with titles from the database
 """
       self.liststore1.clear()
-      for title in self.get_note_titles():
-        self.liststore1.append((title,))
+      for row in self.get_note_titles():
+        print "titles = %s" % row[0]
+        print "date = %s" % row[1]
+        self.liststore1.append((row[0],))
 
   def search_title_populate_liststore1(self,title):
       """
 populate the liststore with titles from the database
 """
       self.liststore1.clear()
-      for titles in self.get_note_titles(title):
-        self.liststore1.append((titles,))
+      for row in self.get_note_titles(title):
+        print "titles = %s" % row[0]
+        print "date = %s" % row[1]
+        self.liststore1.append((row[0],))
     
   
   def get_note_titles(self,title=""):
@@ -81,18 +117,23 @@ get the title from our sqlite3 database self.notedb
       if not title:
         conn = sqlite3.connect(self.notedb)
         c = conn.cursor()
-        c.execute("select title from notes order by title")
-        r = [ row[0] for row in c ]
+        c.execute("select title,mod_date from notes order by title")
+        row = c.fetchall()
         c.close()
       else:
         conn = sqlite3.connect(self.notedb)
         c = conn.cursor()
-        c.execute("select title from notes where title like ?",(title + "%",))
-        #c.execute("select title from notes order by title")
-        r = [ row[0] for row in c ]
+        c.execute("select title,mod_date from notes where title like ?",(title + "%",))
+        row = c.fetchall()
         c.close()
-      return r
-  
+      return row
+
+
+  def get_curr_date(self):
+        now = datetime.datetime.now()
+        curr_date = now.strftime("%Y-%m-%d %H:%M")
+        return curr_date
+      
   def get_note_content(self, title=""):
       """
       Give a title get the content of a note from self.notedb
@@ -114,9 +155,13 @@ get the title from our sqlite3 database self.notedb
   
   def create_new_note(self, title, content=""):
       if title:
+        
         conn = sqlite3.connect(self.notedb)
         c = conn.cursor()
-        c.execute("insert into notes values (?, ?)", (title, content))
+
+        mod_date = self.get_curr_date()
+        
+        c.execute("insert into notes values (?, ?, ?)", (title,mod_date,content))
         conn.commit()
         c.close()
         self.populate_liststore1()
@@ -133,11 +178,11 @@ get the title from our sqlite3 database self.notedb
       else:
         pass
 
-  def edit_title(self,title):
+  def edit_title(self,newtitle):
       if self.title:
         conn = sqlite3.connect(self.notedb)
         c = conn.cursor()
-        c.execute("update notes set title = ? where title = ?", (title, self.title))
+        c.execute("update notes set title = ? where title = ?", (newtitle, self.title))
         conn.commit()
         c.close()
     
@@ -145,7 +190,8 @@ get the title from our sqlite3 database self.notedb
   def on_window1_destroy(self,widget,data=None):
       self.save_note()
       gtk.main_quit()
-  
+
+ 
   def on_treeview1_row_activated(self, widget, row, col):
       """
       When a title is selected update the textbuffer with its contents
@@ -186,20 +232,28 @@ get the title from our sqlite3 database self.notedb
       
       """
       Edit of Titles
-      """		       
+      """
       if len(titles) == 1:
         title = titles[0]
+        self.title = title
         keyname = gtk.gdk.keyval_name(event.keyval)
         if event.keyval == 65471:
-          self.entry2.set_text(title)
-          self.window2.show()
+          newtitle = self.getNewTitle(title)          
+          if newtitle:
+            self.edit_title(newtitle)
+            self.title = newtitle
+            self.populate_liststore1()
+            self.treeview1_select_title(newtitle)
+            content = self.get_note_content(newtitle)
+            self.textbuffer1.set_text(content)
+            self.textview1.grab_focus()
 
       """
       Deletion of Titles
       """
       for title in titles:
         self.title = title
-        keyname = gtk.gdk.keyval_name(event.keyval)                        
+        keyname = gtk.gdk.keyval_name(event.keyval)
         #print "Key %s (%d) was pressed" % (keyname, event.keyval)
         if event.keyval == 65535:
           self.delete_note()
@@ -243,23 +297,11 @@ get the title from our sqlite3 database self.notedb
     #print "Key %s (%d) was pressed" % (keyname, event.keyval)
     self.liststore1.clear()
     title = widget.get_text()
-    #print "title = %s" % (title)
     self.search_title_populate_liststore1(title)
     if title == "":
       self.populate_liststore1()
     pass
 
-  def on_entry2_key_release_event(self,widget,event):
-  
-    keyname = gtk.gdk.keyval_name(event.keyval)
-    #print "Key %s (%d) was pressed" % (keyname, event.keyval)
-    if event.keyval == 65293:
-      title = widget.get_text()
-      self.window2.hide()
-      self.edit_title(title)
-      self.populate_liststore1()
-      self.entry1.set_text(title)
-    
     
   def save_note(self):
       if self.title:
@@ -268,7 +310,8 @@ get the title from our sqlite3 database self.notedb
         content = self.textbuffer1.get_text(
                     self.textbuffer1.get_start_iter(),
                     self.textbuffer1.get_end_iter())
-        c.execute("update notes set content = ? where title = ?", (content, self.title))
+        mod_date = self.get_curr_date()
+        c.execute("update notes set content = ?,mod_date = ? where title = ?", (content,mod_date,self.title))
         conn.commit()
         c.close()
       else:
@@ -280,3 +323,4 @@ get the title from our sqlite3 database self.notedb
 if __name__ == "__main__":
   app = GNotey()
   gtk.main()
+
